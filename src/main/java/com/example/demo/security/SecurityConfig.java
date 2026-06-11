ce0cf8b84b4cb6f09823dc8c678d07fd75378e00
Duckenjy <174082782+Duckenjy28@users.noreply.github.com>
2026-06-11 10:00:56 +0700

    add swagger

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -32,8 +32,12 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
 
                         .requestMatchers(
-                                "/api/auth/**"
+                                        "/api/auth/**",
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html",
+                                        "/v3/api-docs/**"
                         ).permitAll()
+
 
                         .requestMatchers(
                                 "/api/admin/**"
